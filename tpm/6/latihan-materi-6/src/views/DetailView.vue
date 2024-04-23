a5bc629a55807dfc1cd6d54ad72775822e734f5c
fajarrosi <fajarilhamrosi@gmail.com>
2024-04-23 21:52:43 +0700

    feat(tpm-6):finish tpm 6

---
+++ tpm/6/latihan-materi-6/src/views/DetailView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="p-6">
+    <img
+      :src="detailNews.urlToImage"
+      alt="logo-news"
+      class="w-full h-[500px] mb-4"
+    />
+    <div class="text-4xl font-bold text-center mb-3">
+      {{ detailNews.title }}
+    </div>
+    <div class="flex items-center text-center gap-x-4 justify-center mb-6">
+      <div class="text-author font-semibold text-xl">
+        {{ detailNews.author }}
+      </div>
+      <div class="text-author font-semibold text-xl">
+        {{ detailNews.publishedAt }}
+      </div>
+    </div>
+
+    <div class="link-news mb-6">
+      <a
+        :href="detailNews.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="underline text-sky-700"
+        >Klik here to read more about this news</a
+      >
+    </div>
+
+    <button
+      class="p-2 bg-red-500 hover:bg-red-700 rounded-lg text-white"
+      @click="goBack"
+    >
+      Kembali
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailView",
+  computed: {
+    detailNews() {
+      return this.$store.getters.getNewsByTitle(this.$route.params.title);
+    },
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
---
+++ tpm/6/latihan-materi-6/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1 class="text-4xl text-center p-4 font-semibold text-sky-500">
+      Berita Terkini
+    </h1>
+
+    <div class="container-news px-3 py-4" v-if="news.length > 0">
+      <item-news v-for="(item, index) in news" :key="index" v-bind="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemNews from "@/components/ItemNews.vue";
+export default {
+  name: "HomeView",
+  components: {
+    ItemNews,
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.$store.dispatch("saveNews");
+    },
+  },
+  computed: {
+    news() {
+      return this.$store.state.news;
+    },
+  },
+};
+</script>
+
+<style>
+@import url("../assets/home.css");
+</style>
